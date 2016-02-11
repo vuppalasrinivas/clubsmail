2a34db38536a777148e869a81daf52afd6bfba9c
srinivas <srinivas08478@gmail.com>
2016-02-11 19:27:50 +0530

    added search

--- app/controllers/club_controller.rb
+++ app/controllers/club_controller.rb
@@ -1,14 +1,22 @@
 class ClubController < ApplicationController
   def index
 	require 'net/http'
-        source = 'http://holmesplace.pt/pt/api/asDFHASsdjk578bysyxc/dsp_clubinformations/get/json/'
+        source = 'http://holmesplace.pt/pt/api/asDFHASsdjk578bysyxc/dsp_clubinformations/get/json?'
+        if params[:search]
+        unless params[:search].empty?
+            source = source + "name=" + params[:search]
+        end
+        end
         resp = Net::HTTP.get_response(URI.parse(source))
         data = resp.body
         result = JSON.parse(data)
         @club = []
         @club =  result.map{|x| x["ddAddress"]}
         p "..........."
-        p @club
+        if @club.empty?
+            flash[:notice] = "No results were found for your search"
+        end
         p ".........."
+        
   end
 end
