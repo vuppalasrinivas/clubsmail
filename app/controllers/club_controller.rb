1dd46ffbcd1ed7d2bef097c6800ef650ce40b34a
Pradeep Kumar <namanipradeepkumar@gmail.com>
2016-02-16 19:40:56 +0530

    changed api

--- app/controllers/club_controller.rb
+++ app/controllers/club_controller.rb
@@ -1,24 +1,20 @@
 class ClubController < ApplicationController
-  def index
-	require 'net/http'
-        source = 'http://holmesplace.pt/pt/api/asDFHASsdjk578bysyxc/dsp_clubinformations/get/json?'
+    def index
+    	require 'net/http'
+        source = 'http://holmesplace.pt/pt/api/asDFHASsdjk578bysyxc/hp_clubs/get/json/?isClub=1&getData=gps,name&pageBy=100'
         if params[:search]
-        unless params[:search].empty?
-            source = source + "name=" + params[:search]
+            unless params[:search].empty?
+                source = source + "name=" + params[:search]
+            end
         end
-        end
+
         resp = Net::HTTP.get_response(URI.parse(source))
         data = resp.body
         result = JSON.parse(data)
         @club = []
-        @club =  result.map{|x| x["ddAddress"]}
-        p "..........."
-        p @club
-        p ".........."
+        @club =  result.map{|x| x["gps"]}
         if @club.empty?
             flash[:notice] = "No results were found for your search"
         end
-        
-        
-  end
+    end
 end
